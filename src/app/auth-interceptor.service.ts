c61a480fa90aeceeb1ca727a06d3c3ff74a685f1
Daniel Benzion <daniel.z.benzion@gmail.com>
2021-11-08 22:13:33 -0600

    Manipulating request objects

--- src/app/auth-interceptor.service.ts
+++ src/app/auth-interceptor.service.ts
@@ -9,6 +9,9 @@ export class AuthInterceptorService implements HttpInterceptor {
   constructor() { }
 
   intercept(req: HttpRequest<any>, next: HttpHandler) {
+    const modifiedRequest = req.clone({
+      headers: req.headers.append('Auth', 'xyz')
+    });
     return next.handle(req);
   }
 }
