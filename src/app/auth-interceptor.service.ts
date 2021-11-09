f9f818b2b8b532e7f5a049beab4f897cc9eb1a5e
Daniel Benzion <daniel.z.benzion@gmail.com>
2021-11-08 22:09:43 -0600

    Interceptors

---
+++ src/app/auth-interceptor.service.ts
@@ -0,0 +1,14 @@
+import { HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthInterceptorService implements HttpInterceptor {
+
+  constructor() { }
+
+  intercept(req: HttpRequest<any>, next: HttpHandler) {
+    return next.handle(req);
+  }
+}
